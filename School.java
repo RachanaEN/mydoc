b52b45e7a3a8ec377981b6bafe387998272efb67
RachanaEN <142745771+RachanaEN@users.noreply.github.com>
2023-08-21 14:38:29 +0530

    Add files via upload

---
+++ School.java
@@ -0,0 +1,13 @@
+class School
+{
+static String school_name="Amar vidya Mandir" ;
+static String school_grade="A+";
+static int school_strength=1000;
+public static void main(String[] args)
+{
+System.out.println(" Name of the school :"+school_name);
+System.out.println("school grade:"+school_grade );
+System.out.println("school strength:"+school_strength);
+//System.out.println("model of the mobile :"+ car_type);//
+}
+}
